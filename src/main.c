128b9152dc8bf9efb4e5e7b1bd62615681ef8395
Oskari <osktee@gmail.com>
2014-01-26 23:49:39 +0200

    Added some PWM code.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "lcd_lib.h"
 
 #define T1_PS 8
+#define MS2OCR(x) x*2;
 
 #define SHOW_1_DEC 4    // how many decimals we're showing
 #define SHOW_2_DEC 5
@@ -25,26 +26,40 @@
 
 char* Double2Chars(double num);
 
-static char buf[SHOW_3_DEC] = ""; // for double2chars conversion
+static char buf[SHOW_6_DEC] = ""; // for double2chars conversion
 
 int main(void) {
     DDRB = 0xFF; // PWM
-    PORTB = 0xFF;
+    PORTB = 0x00;
 
     LCDinit();
     LCDclr();
-
     _delay_ms(100); // LCD start time
 
-    TCCR1A |= 1<<COM1A1 | 1<<WGM11; // inverted fast PWM: (1<<COM1A0)
+    TCCR1A |= 1<<COM1A1 | 1<<COM1A0 | 1<<WGM11; // inverted fast PWM: (1<<COM1A0)
     TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS11; // prescaler: 8
-    ICR1 = 39999; // 40 000 = 20 ms
-    OCR1A = 20000;
+    ICR1 = 43399;
+
+    /* Pause pulse width 20 ms + control pulse width 1.7 ms (max) = 21.7 ms.
+     * 43 400 in OCR1 = 21.7 ms.
+     *
+     * Possible OCR1 values: 43000 - 43399.
+     * .---------------.-------.---------.-------.
+     * | Rotation:     | CW    | STEADY  | CCW   |
+     * |---------------.--------.--------.-------.
+     * | OCR1A value:  | 43000 | 43200   | 43399 |
+     */
 
     while (1) {
-        LCDGotoXY(LCD_VALUE_SLOT_4);
+        LCDGotoXY(LCD_VALUE_SLOT_1);
         LCDstring( (uint8_t*)Double2Chars(M_PI), (uint8_t)sizeof(buf) - 1);
-        _delay_ms(250);
+
+        OCR1A = 43270;
+        _delay_ms(550);
+
+        OCR1A = 43330;
+        _delay_ms(550);
+
     }
 }
 
